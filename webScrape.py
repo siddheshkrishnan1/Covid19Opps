597a5247abecab14626fa3b4d93e520c7422921d
Siddhesh Krishnan <siddheshkrishnan1@gmail.com>
2020-05-28 17:45:18 -0500

    Added seperate sheets for diff categories, major clean-up required

--- webScrape.py
+++ webScrape.py
@@ -10,9 +10,17 @@ import stringdist
 r = Rake()
 
 
-drivers = {"Technology/Computer Science": ["technical", "AWS", "software", "development", "cyber", "python", "parameter", "computer", "model", "predictive", "data", "machine", "artificial intelligence", "map"], 
+drivers = {"Technology and Computer Science": ["technical", "AWS", "software", "development", "cyber", "python", "parameter", "computer", "model", "predictive", "data", "machine", "artificial intelligence", "map"], 
            "Biomedical": ["biology", "ventilator", "lung", "FDA", "hospital", "medicine","tissue", "nasal", "oropharyngeal", "specimen", "oxygen", "placebo", "virus", "antibiotic", "infection", "chronic", "health", "drug", "swab", "blood", "heart", "genome", "clinical"]}
            #"Social Services": ["state", "lockdown", "environment", "community", "social", "low income", "distancing", "pandemic"]}
+
+catTables = {'Technology and Computer Science': [],
+              'Biomedical': [],
+              'Other': []}
+
+for keys in catTables:
+    for i in range(0, 3):
+        catTables[keys].append([])
 
 def stanford():
 
@@ -31,7 +39,12 @@ def stanford():
             contacts.append(entryVals[1])
             desc.append(entryVals[2])
             r.extract_keywords_from_text(entryVals[2])
-            keyWords.append(getKeyWord(r.get_ranked_phrases()))
+            keyPhrase = getKeyWord(r.get_ranked_phrases())
+            keyWords.append(keyPhrase)
+            catTables[keyPhrase][0].append(entryVals[0])
+            catTables[keyPhrase][1].append("Stanford")
+            catTables[keyPhrase][2].append(entryVals[2])
+
 
 
 
@@ -103,7 +116,11 @@ def virginiaTech():
         typeOfResearch.append(entryVals[4])
         desc.append(descs[index].text)
         r.extract_keywords_from_text(descs[index].text)
-        keyWords.append(getKeyWord(r.get_ranked_phrases()))
+        keyPhrase = getKeyWord(r.get_ranked_phrases())
+        keyWords.append(keyPhrase)
+        catTables[keyPhrase][0].append(entryVals[2])
+        catTables[keyPhrase][1].append("Virginia Tech")
+        catTables[keyPhrase][2].append(descs[index].text)
         index+=2
 
     index = 1
@@ -113,7 +130,11 @@ def virginiaTech():
         typeOfResearch.append(entryVals[4])
         desc.append(descs[index].text)
         r.extract_keywords_from_text(descs[index].text)
-        keyWords.append(getKeyWord(r.get_ranked_phrases()))
+        keyPhrase = getKeyWord(r.get_ranked_phrases())
+        keyWords.append(keyPhrase)
+        catTables[keyPhrase][0].append(entryVals[2])
+        catTables[keyPhrase][1].append("Virginia Tech")
+        catTables[keyPhrase][2].append(descs[index].text)
         index+=2
 
 
@@ -125,8 +146,15 @@ def virginiaTech():
     df.to_csv('VirginiaTechProjects.csv', index = False)
     return df
 
-stanford()
-virginiaTech()
+def turnCatstoFiles():
+    for keys in catTables:
+        fileName = str(keys)+".csv"
+        df = pd.DataFrame(list(zip(catTables[keys][0], catTables[keys][1], catTables[keys][2])), 
+                    columns =['Project Title', 'Source', 'Project Description']) 
+        df.to_csv(fileName, index = False)
+
+
+   
 
 stanVal = pd.read_csv('StanfordProjects.csv')
 
@@ -152,10 +180,48 @@ vTTab = go.Figure(data=[go.Table(
 ])
 
 
+techVal = pd.read_csv('Technology and Computer Science.csv')
+
+techTab = go.Figure(data=[go.Table(
+    header=dict(values=list(techVal.columns),
+                fill_color='green',
+                align='left'),
+    cells=dict(values=[techVal[k].tolist() for k in techVal.columns[0:]],
+               fill_color='white',
+               align='left'))
+])
+
+
+
+bioVal = pd.read_csv('Biomedical.csv')
+
+bioTab = go.Figure(data=[go.Table(
+    header=dict(values=list(bioVal.columns),
+                fill_color='pink',
+                align='left'),
+    cells=dict(values=[bioVal[k].tolist() for k in bioVal.columns[0:]],
+               fill_color='white',
+               align='left'))
+])
+
+
+
+otherVal = pd.read_csv('Other.csv')
+
+otherTab = go.Figure(data=[go.Table(
+    header=dict(values=list(otherVal.columns),
+                fill_color='grey',
+                align='left'),
+    cells=dict(values=[otherVal[k].tolist() for k in otherVal.columns[0:]],
+               fill_color='white',
+               align='left'))
+])
+
+
 
 
 st.title('Covid-19 Research Opportunities')
-oppVals = {'Stanford': stanTab, 'Virginia Tech': vTTab}
+oppVals = {'Stanford': stanTab, 'Virginia Tech': vTTab, "Technology": techTab, "Biomedical": bioTab, "Other": otherTab}
 val = st.selectbox("Opportunity Choices", list(oppVals.keys()), 0)
 st.plotly_chart(oppVals[val])
 

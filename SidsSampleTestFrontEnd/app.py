9532bad479fcbcea8f89339e4547c60a28db9831
Siddhesh Krishnan <siddheshkrishnan1@gmail.com>
2020-07-20 18:29:17 -0500

    Added tabs

--- SidsSampleTestFrontEnd/app.py
+++ SidsSampleTestFrontEnd/app.py
@@ -2,9 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
-
-app = dash.Dash()
 
 card = dbc.Card(
     [
@@ -22,7 +21,19 @@ card = dbc.Card(
     style={"width": "18rem"},
 )
 
-app.layout = html.Div(children=[
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Tabs(id='tabs-example', value='tab-1', children=[
+        dcc.Tab(label='Tab one', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+    html.Div(id='tabs-example-content')
+])
+
+
+valA = html.Div(children=[
     html.H1(children='My Boredom in Quarentine'),
         dcc.Graph(
         id='example',
@@ -41,6 +52,17 @@ app.layout = html.Div(children=[
     ])])
 
 
+@app.callback(Output('tabs-example-content', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return valA
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('More stuff here')
+        ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
